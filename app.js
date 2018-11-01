0d5b8d18896851f1e057078f68209fe42f6f3d44
Dragos Sarpe <contact@usdgadget.com>
2018-11-01 14:11:19 +0200

    Added the EventListener for Roll the Dice button

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+/*
+  btn is called a callback function - it's a function that isn't called by us,
+  but by another function. It is a funtion that we pass into another function,
+  as an argument and here the EventListener method will then call that function
+  for us.
+*/
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+	// 1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	// 2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+});
