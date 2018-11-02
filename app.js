29e21a8fa058640b2e376e37914268934a11d53c
Dragos Sarpe <contact@usdgadget.com>
2018-11-02 10:15:17 +0200

    Added event listener for Hold button

--- app.js
+++ app.js
@@ -36,6 +36,30 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		roundScore += dice;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
+		nextPlayer();
+	}
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	// Add current score to global score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// Check if the player won the game
+	if (scores[activePlayer] >= 100) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		// Next player
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 		roundScore = 0;
 		document.getElementById('current-0').textContent = '0';
@@ -45,5 +69,4 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		document.querySelector('.player-1-panel').classList.toggle('active');
 
 		document.querySelector('.dice').style.display = 'none';
-	}
-});
+}
